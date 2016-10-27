510f703774597909594a6bf1df6edc0ff65e2de0
toohamster <449211678@qq.com>
2016-10-27 15:40:08 +0800

    modify test demo

--- tests/Ws/Mvc/App/Blog/__init.php
+++ tests/Ws/Mvc/App/Blog/__init.php
@@ -13,21 +13,23 @@ Env::classLoader()->addPsr4('Blog\\', $dir);
 
 // 绑定命令
 Cmd::id('hello')->bind(Request::GET, function($app){
+	output('App: ' . $app->config()->get('app.id'), '');
+
 	output('Hello World!', 'text');
 	output($app->pagePathing('who.are.you',['name'=>'a test']), 'url');
 	output($app->jsonPathing('who.love.you',['tag'=>'php']), 'url');
-});
+})->bindTo($app);;
 
 Cmd::id('index')->bind(Request::GET, 'Blog\Controller\Index@index')->bindTo($app);
 
-Cmd::group([
-		[
-			'id'	=> 'hello',
-			'event'	=> Request::GET,
-			'closure'	=> function($a){
-				Env::dump($a);
-			}
-		]
-	])->bindTo($app);
+// Cmd::group([
+// 		[
+// 			'id'	=> 'hello',
+// 			'event'	=> Request::GET,
+// 			'closure'	=> function($a){
+// 				Env::dump($a);
+// 			}
+// 		]
+// 	])->bindTo($app);
 
 // Env::dump($app);
---
+++ tests/Ws/Mvc/App/Default/__init.php
@@ -0,0 +1,21 @@
+<?php
+use Ws\Env;
+use Ws\Mvc\Request;
+use Ws\Mvc\Cmd;
+
+$app = $this->me();
+/*@var $app \Ws\Mvc\App */
+
+$dir = $app->config()->get('app.dir');
+
+// 注入类路径
+Env::classLoader()->addPsr4('Default\\', $dir);
+
+// 绑定命令
+Cmd::id('index')->bind(Request::GET, function($app){
+	output('App: ' . $app->config()->get('app.id'), '');
+
+	output('Hello World!', 'text');
+	output($app->pagePathing('who.are.you',['name'=>'a test']), 'url');
+	output($app->jsonPathing('who.love.you',['tag'=>'php']), 'url');
+})->bindTo($app);
